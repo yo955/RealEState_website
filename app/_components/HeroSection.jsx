b74df02523a3648031ccff2029796eb43226362c
Joo <youssefjoo@gmail.com>
2024-10-20 22:53:00 +0300

    responsive

--- app/_components/HeroSection.jsx
+++ app/_components/HeroSection.jsx
@@ -6,7 +6,7 @@ const HeroSection = () => {
       <div className="max-w-full mx-auto flex flex-col items-start">
         <TypingAnimation />
         <div className="w-full sm:w-[70%] lg:w-[63%] pt-[43px] pb-[30px]">
-          <h1 className="text-xl sm:text-2xl lg:text-3xl text-primary text-right leading-relaxed">
+          <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl text-primary leading-relaxed text-justify">
             نوفر لك امتلاك سكنك المثالي المتميز بتصاميم عصرية وتقنيات حديثة،
             <br />
             لتعيش حياة أفضل في منزلك براحة وأمان
